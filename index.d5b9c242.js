6b20e4d87d80d3af42d195da5c6e02fa3382b7bc
Nazarii <Kyzenko.nr@gmail.com>
2024-06-20 12:22:01 +0300

    make build

---
+++ index.d5b9c242.js
@@ -0,0 +1,2 @@
+document.querySelector(".spider").style.cssText="\n  top: 50%;\n  left: 50%;\n  transform: translate(-50%, -50%);\n";
+//# sourceMappingURL=index.d5b9c242.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+// write code here
+const spider = document.querySelector(".spider");
+spider.style.cssText = `
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
